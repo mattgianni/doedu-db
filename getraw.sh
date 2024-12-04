0ded7545ef3f13b5e82493c88e64d1e8dc12c4d5
Matthew Gianni <matt@gianni.org>
2024-12-04 10:44:07 -0800

    added grads table + DL scripts

--- getraw.sh
+++ getraw.sh
@@ -20,8 +20,13 @@ if [ ! -f $RAW_DIR/StudentGroups.txt ]; then
     unzip $RAW_DIR/StudentGroups.zip -d $RAW_DIR && rm $RAW_DIR/StudentGroups.zip
 fi
 
+if [ ! -f $RAW_DIR/fycgr24.txt ]; then
+    curl -Lo $RAW_DIR/fycgr24.txt https://www3.cde.ca.gov/demo-downloads/fycgr/fycgr24.txt
+fi
+
 # convert the file to a properly formatted utf-8 standard and convert '*' to ''
 iconv -f utf-8 -t utf-8 -c $RAW_DIR/sb_ca2024_all_csv_v1.txt | sed 's/\*//g' > $RAW_DIR/scores.dat
 iconv -f utf-8 -t utf-8 -c $RAW_DIR/sb_ca2024entities_csv.txt | sed 's/\*//g' > $RAW_DIR/entities.dat
 iconv -f utf-8 -t utf-8 -c $RAW_DIR/StudentGroups.txt | sed 's/\*//g' > $RAW_DIR/demographics.dat
 iconv -f utf-8 -t utf-8 -c $RAW_DIR/Tests.txt | sed 's/\*//g' > $RAW_DIR/tests.dat
+iconv -f utf-8 -t utf-8 -c $RAW_DIR/fycgr24.txt | sed 's/\*//g' | sed 's/\t/^/g' > $RAW_DIR/fycgr24.dat
